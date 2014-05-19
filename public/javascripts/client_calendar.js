179b5eade2e1339bb9fd451a7113ab850652fe5b
ccarreon <christianjc123@gmail.com>
2014-05-19 12:26:37 -0700

    Implemented getCurrentDayAsIndex

--- public/javascripts/client_calendar.js
+++ public/javascripts/client_calendar.js
@@ -46,10 +46,118 @@ ClientCalendar.prototype.getAvaliableShiftsAtDayIndex = function(day_index) {
     return null
 }
 
+// This function returns a number that corresponds with a specific date
+// in order to generate a day index
 ClientCalendar.prototype.getCurrentDayAsIndex = function() {
+    //Javascript Date object
     var currentDay = new Date()
-    console.log('testing getCurrentDayAsIndex')
-    // TODO: Calculate and return current day as an index
+
+    //Get day of the month (from 1-31)
+    var day = currentDay.getDate()
+    //Get month of the year (from 0-11)
+    var month = currentDay.getMonth()
+    //Get year (four digits)
+    var year = currentDay.getFullYear()
+    //Index to be returned 
+    var dayIndex 
+    
+    //Determines if the current year is a leap year
+    if((year%4) === 0) {
+      var leapYear = true
+    }else{
+      var leapYear = false
+    }
+    
+    //Calculates months for NOT a leap year
+    if(!leapYear) {
+      switch(month) {
+      case 0:
+         dayIndex = 0
+         break;
+      case 1:
+         dayIndex = 31
+         break;
+      case 2:
+         dayIndex = 59
+        break;
+      case 3:
+         dayIndex = 90
+         break;
+      case 4:
+         dayIndex = 120
+         break;
+      case 5:
+         dayIndex = 151
+         break;
+      case 6:
+         dayIndex = 181
+         break;
+      case 7:
+         dayIndex = 212
+         break;
+      case 8:
+         dayIndex = 243
+         break;
+      case 9:
+         dayIndex = 273
+         break;
+      case 10:
+         dayIndex = 304
+         break;
+      case 11:
+         dayIndex = 334
+         break;    
+      default:
+         console.log('[+] Error: No month given')
+       }
+    }
+    //Calculates months for a leap year
+    if(leapYear) {
+      switch(month) {
+      case 0:
+         dayIndex = 0
+         break;
+      case 1:
+         dayIndex = 31
+         break;
+      case 2:
+         dayIndex = 60
+        break;
+      case 3:
+         dayIndex = 91
+         break;
+      case 4:
+         dayIndex = 121
+         break;
+      case 5:
+         dayIndex = 152
+         break;
+      case 6:
+         dayIndex = 182
+         break;
+      case 7:
+         dayIndex = 213
+         break;
+      case 8:
+         dayIndex = 244
+         break;
+      case 9:
+         dayIndex = 274
+         break;
+      case 10:
+         dayIndex = 305
+         break;
+      case 11:
+         dayIndex = 335
+         break;    
+      default:
+         console.log('[+] Error: No month given')
+       }
+    }
+    //Day index is finalized
+    dayIndex = dayIndex + day
+
+    return dayIndex
 
 } // end of getCurrentDayAsIndex()
 
