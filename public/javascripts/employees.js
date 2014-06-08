d18ce450f82415a32874606eebe2edbbf3133bcb
nardecky <nardecky@ucsd.edu>
2014-06-08 02:35:14 -0700

    Fixed employee merge conflicts. Added delete employee button

--- public/javascripts/employees.js
+++ public/javascripts/employees.js
@@ -38,7 +38,7 @@ $(document).ready(function() {
    //need to grab all current emloyees
   socket.emit('retrieve-all-employees', function(err, employees)
   {
-<<<<<<< Updated upstream
+
     var totalEditButtonsActive = 0
     console.log('Retrieve employees...')
     //console.log(JSON.stringify(employees))
@@ -153,7 +153,7 @@ $(document).ready(function() {
       var cancel = "#"+id+" .cancel"
       var edit = "#"+id+" .edit"
       var input = "#"+id+" input"
-      var deleteUser = '#'+id+'.deleteUser'
+      var deleteUser = "#"+id+".deleteUser"
 
       $(input).closest('form').find("input[type=text], textarea").val("");
 
@@ -164,76 +164,5 @@ $(document).ready(function() {
     })
   })
 })
-=======
-      console.log('Retrieve employees...')
-      //console.log(JSON.stringify(employees))
 
-      // Sort employees
-      employees.sort(propertyCompare('username'))
-      
-      for( var i = 0; i < employees.length; ++i) {
-        // employeeInfo = { "username" : userName, "phonenumber" : phoneNumber,
-        //                  "role" : role, "email" : email }
-        var id = "id="+i;
-        var name = employees[i].username
-        //var phone = employees[i].phoneNumber
-        var email = employees[i].email
-        var position = employees[i].role
-        console.log(employees.length)
-
-        $(".box").append( '<form '+id+'><div class="name" '+id+'><h2>'+name+'</h2></div><input type="text", placeholder="CHANGENAME", name="name" value="'+name+'">\
-          </input><div class="email" '+id+'><h2>'+email+'</h2></div><input type="text", placeholder="NEW EMAIL", name="email" value=""></input>\
-          <div class="role" '+id+'><h2>'+position+'</h2></div><div class="edit" '+id+'>EDIT</div><div class="submit" '+id+' >SUBMIT</div><div class="cancel" '+id+'>CANCEL</div></form>')
-     }
-   
-      //initally hid button divs
-       $(".submit").hide()
-       $(".cancel").hide()
-
-       //hide inputs
-       $("input").hide()
-
-
-     //on edit click
-     $(".edit").click(function()
-     {
-       console.log("Made it")
-       var id = $(this).closest("div").attr("id");
-       var submit = "#"+id+".submit"
-       var cancel = "#"+id+".cancel"
-       var edit = "#"+id+".edit"
-       var form = "#"+id+" input"
-       $(''+id+' > h2:contains('+name+')').hide()
-       //$('#BrandContent > h2:contains("All Brands")').css('visibility', 'hidden')
-       $(edit).hide()
-       $(form).show()
-       $(submit).show()
-       $(cancel).show()
-      
-       //set focus to prompt name change
-       //$('cursor').focus()
-     })
-
-
-   //on cancel click
-   $(".cancel").click(function()
-   {
-     console.log("Made it")
-     var id = $(this).closest("div").attr("id");
-     
-     var submit = "#"+id+".submit"
-     var cancel = "#"+id+".cancel"
-     var edit = "#"+id+".edit"
-     var form = "#"+id+" input"
-     $(form).hide()
-     $(cancel).hide()
-     $(submit).hide()
-     $(edit).show()
-
-   })
-
-   })
-
-
-});
->>>>>>> Stashed changes
+ 
